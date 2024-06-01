77c5f2108d0f11244c4a2d405cd33f35273289ab
Josue Ruiz <106292389+Josue-R3@users.noreply.github.com>
2024-05-31 19:37:40 -0500

    intento continuar header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from 'next/link';
 import Image from "next/image";
 import NavLink from '../content/NavLink';
+import { Button } from "@nextui-org/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const navLinks = [
     {
@@ -24,13 +26,19 @@ const navLinks = [
         title: 'Contact',
         path: '#contact'
     },
-]
+];
 
 export default function Header() {
+    const [headerOpen, setHeaderOpen] = useState(false);
+
+    const toggleHeader = () => {
+        setHeaderOpen(!headerOpen);
+    };
+
     return (
-        <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90 " >
-            <div className="flex flex-wrap items-center justify-between mx-auto px-4" >
-                <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold" >
+        <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+            <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
+                <Link href="/" className="text-2xl md:text-5xl text-white font-semibold">
                     <Image
                         src='/images/logo_v4.png'
                         alt="logo"
@@ -38,15 +46,23 @@ export default function Header() {
                         height={50}
                     />
                 </Link>
-                <div className="menu hidden md:block md:w-auto" id="navbar" >
-                    <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0" >
-                        {
-                            navLinks.map((link, index) => (
-                                <li key={index}>
-                                    <NavLink href={link.path} title={link.title} ></NavLink>
-                                </li>
-                            ))
-                        }
+                <div className="mobile-menu block md:hidden">
+                    <Button
+                    variant="bordered"
+                        onClick={toggleHeader}
+                        className="flex items-center px-3 py-2 rounded border-white bg-[#121212] text-white hover:bg-opacity-80"
+                        isIconOnly
+                    >
+                        {headerOpen ? <XMarkIcon className="h-5 w-5" /> : <Bars3Icon className="h-5 w-5" />}
+                    </Button>
+                </div>
+                <div className={`menu ${headerOpen ? 'block' : 'hidden'} md:block md:w-auto`} id="navbar">
+                    <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0 text-white">
+                        {navLinks.map((link, index) => (
+                            <li key={index}>
+                                <NavLink href={link.path} title={link.title} />
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
