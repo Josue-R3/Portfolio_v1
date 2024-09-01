34cafaa5a1c9ee3909e7e3669dcbc82e355aba7a
Josue Ruiz <106292389+Josue-R3@users.noreply.github.com>
2024-09-01 16:47:45 -0500

    tooltip skills

--- src/components/section/Skills.jsx
+++ src/components/section/Skills.jsx
@@ -1,5 +1,4 @@
-// components/section/Skills.js
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { Tooltip } from "@nextui-org/react";
 
@@ -20,24 +19,59 @@ const TECHNOLOGIES = [
   { name: "Elementor", image: "/images/logos/elementor.png" },
 ];
 
-const Skills = () => (
-  <div className="grid grid-cols-3 md:grid-cols-7 gap-8">
-    {TECHNOLOGIES.map((tech) => (
-      <div key={tech.name} className="flex justify-center items-center">
-        <Tooltip content={tech.name} >
-        <Image
-          src={tech.image}
-          alt={tech.name}
-          width={50}
-          height={50}
-          className="w-12 h-12"
-          //className="w-12 h-12 filter grayscale hover:grayscale-0 transition duration-100 "
-          style={tech.customStyle ? tech.customStyle : {}}
-          />
+const Skills = () => {
+  const [activeIndex, setActiveIndex] = useState(-1);
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+
+  // Maneja el evento de movimiento del mouse
+  const handleMouseMove = (index) => {
+    setActiveIndex(index);
+    setTooltipVisible(true); // Mostrar el tooltip inmediatamente
+  };
+
+  // Resetea el estado cuando el cursor sale del contenedor
+  const handleMouseLeave = () => {
+    setActiveIndex(-1);
+    setTimeout(() => setTooltipVisible(false), 300); // Retraso para ocultar el tooltip
+  };
+
+  useEffect(() => {
+    if (activeIndex === -1) {
+      setTooltipVisible(false);
+    }
+  }, [activeIndex]);
+
+  return (
+    <div
+      className="grid grid-cols-3 md:grid-cols-7 gap-8"
+      onMouseLeave={handleMouseLeave}
+    >
+      {TECHNOLOGIES.map((tech, index) => (
+        <div
+          key={tech.name}
+          className="flex justify-center items-center"
+          onMouseMove={() => handleMouseMove(index)}
+        >
+          <Tooltip
+            content={tech.name}
+            placement="top"
+            visible={tooltipVisible && activeIndex === index} // Controla la visibilidad del tooltip
+          >
+            <Image
+              src={tech.image}
+              alt={tech.name}
+              width={50}
+              height={50}
+              className={`w-12 h-12 transition-transform duration-300 transform ${
+                activeIndex === index ? "scale-150" : "scale-100"
+              }`}
+              style={tech.customStyle ? tech.customStyle : {}}
+            />
           </Tooltip>
-      </div>
-    ))}
-  </div>
-);
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Skills;
